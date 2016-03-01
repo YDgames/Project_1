17fcc9861a04a953ccc928498f4f6ccf0925a56a
dudgns0908 <k2110235@hanmail.net>
2016-03-02 02:30:29 +0900

    Move KeyArrow
    Move KeyArrow

--- YD_Project_1/Assets/Cameras.cs
+++ YD_Project_1/Assets/Cameras.cs
@@ -8,16 +8,16 @@ public class Cameras : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		speed = 2f;
+		speed = 5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKey(KeyCode.W)) transform.Translate (transform.forward * Time.deltaTime * speed);
-		else if(Input.GetKey(KeyCode.S)) transform.Translate (-transform.forward * Time.deltaTime * speed);
-		else if(Input.GetKey(KeyCode.A)) transform.Translate (-transform.right * Time.deltaTime * speed);
-		else if(Input.GetKey(KeyCode.D)) transform.Translate (transform.right * Time.deltaTime * speed); 
+		if(Input.GetKey(KeyCode.UpArrow)) transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		if(Input.GetKey(KeyCode.DownArrow)) transform.Translate (-Vector3.forward * Time.deltaTime * speed);
+		if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate (-Vector3.right * Time.deltaTime * speed);
+		if(Input.GetKey(KeyCode.RightArrow)) transform.Translate (Vector3.right * Time.deltaTime * speed); 
 
 	}
 }
