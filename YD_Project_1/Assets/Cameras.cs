249621780041350b5705b08828ef18c5d2e59b50
dudgns0908 <k2110235@hanmail.net>
2016-03-02 02:04:53 +0900

    move Camera
    move Camera

---
+++ YD_Project_1/Assets/Cameras.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class Cameras : MonoBehaviour {
+
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.W)) transform.Translate (transform.forward * Time.deltaTime * speed);
+		else if(Input.GetKey(KeyCode.S)) transform.Translate (-transform.forward * Time.deltaTime * speed);
+		else if(Input.GetKey(KeyCode.A)) transform.Translate (-transform.right * Time.deltaTime * speed);
+		else if(Input.GetKey(KeyCode.D)) transform.Translate (transform.right * Time.deltaTime * speed); 
+
+	}
+}
