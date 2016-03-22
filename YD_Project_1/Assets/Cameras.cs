a3711b0bcdf7991a2d7deeca134c7949335a6db4
dudgns0908 <k2110235@hanmail.net>
2016-03-22 18:36:09 +0900

    test
    test

--- YD_Project_1/Assets/Cameras.cs
+++ YD_Project_1/Assets/Cameras.cs
@@ -14,10 +14,10 @@ public class Cameras : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKey(KeyCode.UpArrow)) transform.Translate (Vector3.forward * Time.deltaTime * speed);
-		if(Input.GetKey(KeyCode.DownArrow)) transform.Translate (-Vector3.forward * Time.deltaTime * speed);
-		if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate (-Vector3.right * Time.deltaTime * speed);
-		if(Input.GetKey(KeyCode.RightArrow)) transform.Translate (Vector3.right * Time.deltaTime * speed); 
+		    if(Input.GetKey(KeyCode.UpArrow)) transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		    if(Input.GetKey(KeyCode.DownArrow)) transform.Translate (-Vector3.forward * Time.deltaTime * speed);
+		    if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate (-Vector3.right * Time.deltaTime * speed);
+		    if(Input.GetKey(KeyCode.RightArrow)) transform.Translate (Vector3.right * Time.deltaTime * speed); 
 
 	}
 }
