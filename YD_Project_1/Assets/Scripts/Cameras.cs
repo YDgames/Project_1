5fd39dc3b2ca1597b9a5954950e787a753200f33
dudgns0908 <k2110235@hanmail.net>
2016-03-22 19:10:18 +0900

    Add Camera Move
    Add Camera Move

--- YD_Project_1/Assets/Scripts/Cameras.cs
+++ YD_Project_1/Assets/Scripts/Cameras.cs
@@ -3,21 +3,17 @@ using System.Collections;
 using System.Timers;
 
 public class Cameras : MonoBehaviour {
-    /*
-	float speed;
+    
+	GameObject player = null;
 
 	// Use this for initialization
 	void Start () {
-		speed = 5f;
+		player = GameObject.Find ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		    if(Input.GetKey(KeyCode.UpArrow)) transform.Translate (Vector3.forward * Time.deltaTime * speed);
-		    if(Input.GetKey(KeyCode.DownArrow)) transform.Translate (-Vector3.forward * Time.deltaTime * speed);
-		    if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate (-Vector3.right * Time.deltaTime * speed);
-		    if(Input.GetKey(KeyCode.RightArrow)) transform.Translate (Vector3.right * Time.deltaTime * speed); 
-
-	}*/
+		Vector3 v3 = player 	.transform.position;
+		transform.position = new Vector3 (v3.x, v3.y + 70, v3.z - 50);
+	}
 }
--- YD_Project_1/Assets/Scripts/Player_Controller.cs
+++ YD_Project_1/Assets/Scripts/Player_Controller.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class Player_Controller : MonoBehaviour {
 
+	float speed;
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
+		speed = 800f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKey(KeyCode.W)) rb.AddForce (Vector3.forward * Time.deltaTime * speed);
+		if(Input.GetKey(KeyCode.S)) rb.AddForce (-Vector3.forward * Time.deltaTime * speed);
+		if(Input.GetKey(KeyCode.A)) rb.AddForce (-Vector3.right * Time.deltaTime * speed);
+		if(Input.GetKey(KeyCode.D)) rb.AddForce (Vector3.right * Time.deltaTime * speed); 
 	}
 }
